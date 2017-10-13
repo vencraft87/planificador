2c95fb76b7a80edc89b917d75378d02a6f3d7c7b
vencraft87 <vencraft87@gmail.com>
2017-10-13 13:41:56 -0300

    Comentado
    Se agregan comentarios y se quita uso de variable i=o en primer while

--- obligatorio.cpp
+++ obligatorio.cpp
@@ -3,6 +3,7 @@
 
 #define	MAX_PROCESOS 20
 
+// Estructura de los procesos
 struct str_proc{
 	int numero;
 	int arribo;
@@ -12,6 +13,7 @@ struct str_proc{
 
 typedef struct str_proc proceso;
 
+//Arreglo que contiene a cada proceso
 struct str_procAT{
 	proceso arr_procesos[MAX_PROCESOS];
 	int tope;
@@ -19,13 +21,18 @@ struct str_procAT{
 
 typedef struct str_procAT AT_Procesos; // arreglo con tope de procesos
 
+/*
+//Posible uso para guardar procesos en estado listo 
 struct str_procListoAT{
 	proceso arr_procesos[MAX_PROCESOS];
 	int tope;
 };
 
 typedef struct str_procListoAT AT_ProcesosListos;
+*/
 
+/*
+//Uso solo de testing para verificar procesos y estados
 void imprimirProcesos(proceso p){
     printf("------------------------\n");
 	printf("Num Proceso: %d\n", p.numero);
@@ -34,8 +41,9 @@ void imprimirProcesos(proceso p){
 	printf("Estado: %d\n", p.estado);
     printf("------------------------\n");
 }
-
-void imprimirProcesoEnUso(AT_Procesos &atp){
+*/
+//Imprime uso de CPU; procesos en ejecucion en un tiempo determinado.
+void imprimirUsoCpu(AT_Procesos &atp){
 	int i = 0;
 	bool ejecuta = false;
 	while (i < atp.tope && ejecuta != true){
@@ -53,7 +61,7 @@ void imprimirProcesoEnUso(AT_Procesos &atp){
 		printf("CPU Libre\n");
 	}
 }
-
+//Ordena procesos segun la rafaga
 void ordenaRafaga(AT_Procesos &atp){
 	proceso temp;
 	for(int i=0; i<atp.tope-1; i++){
@@ -71,17 +79,16 @@ main(){
 	AT_Procesos procesos;
 	procesos.tope = 0;
 	proceso p;
-	
+	p.numero = 1;
 	int num_proc, arribo, rafaga;
 	char enter;
+	
+	//Solicita el ingreso de los procesos y los carga en el array
 	printf("Seleccione cantidad de procesos a ejecutar: ");
 	scanf("%d%c",&num_proc,&enter);
 	printf("\n");
-	
-	int i=1;
 	while (num_proc > 0){
-		p.numero = i;
-		printf("Proceso %d:\n\n",i++);
+		printf("Proceso %d:\n\n",p.numero);
 		printf("Ingrese el tiempo de arribo: ");
 		scanf("%d%c",&p.arribo,&enter);
 		printf("Ingrese el tiempo de rafaga: ");
@@ -90,18 +97,20 @@ main(){
 		p.estado = 0;
 		procesos.arr_procesos[procesos.tope] = p;
 		procesos.tope++;
+		p.numero++;
 		num_proc--;
 	}
-	
+	//Por cada segundo recorre el array de procesos para ponerlos en estado listo
 	for(int i=0;i < 50; i++){ // resolver de otra forma cuanto tiempo revisar procesos.
 		for(int j=0;j<procesos.tope;j++){
 			if(procesos.arr_procesos[j].arribo == i && procesos.arr_procesos[j].estado != 3){
 				procesos.arr_procesos[j].estado = 1;
 			}
 		}
+		//ordena procesos por rafaga, y ejecuta los listos
 		ordenaRafaga(procesos);
 		printf("----------------\n");
 		printf("%d: ",i);		
-		imprimirProcesoEnUso(procesos);
+		imprimirUsoCpu(procesos);
 	}
 }
